38442b577ec2fbe9fc69f73a67c13b80b0bfb37c
cipher <157321129+cipher0xdev@users.noreply.github.com>
2024-02-22 07:06:28 -0300

    Update core.sh

--- core.sh
+++ core.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
-   echo "the script must be run as root"
-   exit 1
-fi
+require_root() {
+   if [[ $EUID -ne 0 ]]; then
+      echo "the script must be run as root"
+      exit 1
+   fi
+}
