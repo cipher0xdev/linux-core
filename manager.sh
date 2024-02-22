943c015c8e91a1da039679ee099f370181ba9e6f
cipher <157321129+cipher0xdev@users.noreply.github.com>
2024-02-22 06:54:28 -0300

    Update manager.sh

--- manager.sh
+++ manager.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
-   echo "the script must be run as root"
-   exit 1
-fi
+source "core.sh"
 
 # usage: dpkg_check "libname"
 dpkg_check() {
   dpkg -s $1 >/dev/null 2>&1 && installed=true || installed=false 
 
   if [ "$installed" == false ]; then
-    echo "installing $1..."
     apt update > /dev/null 2>&1 
     apt install $1 > /dev/null 2>&1 
-  else 
-    echo "$1 installed sucessful"
   fi
 }
 
